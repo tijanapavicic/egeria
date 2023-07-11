fd7a31d42ed940ba1cc72bc3c083008e5a581d6b
tijanapavicic <tijana.pavicic@ing.com>
2023-07-11 12:50:21 +0200

    [omag server] initial refactoring and first tests

---
+++ open-metadata-implementation/server-chassis/server-chassis-spring/src/test/java/org/odpi/openmetadata/server/config/AvailabilityProbesAutoConfigurationTest.java
@@ -0,0 +1,26 @@
+package org.odpi.openmetadata.server.config;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by tijana.pavicic on egeria
+ * on 10/07/2023 at 16:11
+ */
+class AvailabilityProbesAutoConfigurationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void livenessStateHealthIndicator() {
+    }
+}
